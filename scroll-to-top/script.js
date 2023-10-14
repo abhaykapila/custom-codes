84a75d8c0aef615c2e4d31055b83e58551b06534
Abhay Kapila <abhaykapila@gmail.com>
2023-10-14 15:20:07 +0530

    Scroll-To-Top

---
+++ scroll-to-top/script.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function () {
+    jQuery(window).scroll(function () {
+      if (jQuery(this).scrollTop() > 100) {
+        jQuery('.scroll-top').fadeIn();
+      } else {
+        jQuery('.scroll-top').fadeOut();
+      }
+    });
+  
+    jQuery('.scroll-top').click(function () {
+      jQuery("html, body").animate({
+        scrollTop: 0
+      }, 300);
+        return false;
+    });
+  
+  });
